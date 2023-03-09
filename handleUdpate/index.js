374aa130bf3bea7a24daeb7194f2e231877a81c5
Marco <pastry07@gmail.com>
2023-03-08 17:42:44 -0800

    lab 18 complete

---
+++ handleUdpate/index.js
@@ -0,0 +1,32 @@
+// third party library
+const dynamoose = require('dynamoose');
+
+// create a schema
+const schema = new dynamoose.Schema({
+  "id": String,
+  "name": String,
+  "phone": String,
+});
+
+const peopleModel = dynamoose.model('peoples', schema);
+
+exports.handler = async (event) => {
+  let parsedBody = JSON.parse(event.body);
+  console.log(parsedBody);
+  
+  let id = event.pathParameters.id;
+
+  const response = { statusCode: null, body: null };
+
+  try {
+    let newPerson = await peopleModel.update({ "id": id }, parsedBody);
+    response.body = JSON.stringify(newPerson);
+    response.statusCode = 200;
+  }
+  catch (e) {
+    response.body = JSON.stringify(e.message);
+    response.statusCode = 500;
+  }
+
+  return response;
+};
